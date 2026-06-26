f23c007b38e588f5a82584958d1dbc5f984e821e
devFelina <kushan.s.chamikara@gmail.com>
2026-06-26 13:54:50 +0530

    Change python port

--- microservice/app.py
+++ microservice/app.py
@@ -90,6 +90,8 @@ def predict():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', '5001'))
-    host = os.environ.get('HOST', '127.0.0.1')
+    import os
+    port = int(os.environ.get('PORT', 5001))
+    host = os.environ.get('HOST', '0.0.0.0')
+    
     app.run(host=host, port=port, debug=True)
